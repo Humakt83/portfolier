213cfae4a0ffcfdb2229845681791b9ace603d87
Humakt83 <urlact@yahoo.com>
2017-02-04 02:36:35 +0200

    Bundling WIP...

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 require('gulp-task-loader')();
 const gulp = require('gulp');
 const reload = require('browser-sync').reload;
+const runSequence = require('run-sequence');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
-gulp.task('start', ['clean', 'copy', 'compile', 'serve'], () => {
-    gulp.watch(['src/**/*'], ['clean', 'copy', 'compile', reload]);
+gulp.task('copy:libs', function() {
+    return gulp.src([
+        'node_modules/es6-shim/es6-shim.min.js',
+        'node_modules/es6-promise/dist/es6-promise.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'system.config.js',
+        ])
+        .pipe(concat('vendors.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('start', () => {
+    runSequence('clean', ['copy', 'compile'], 'copy:libs', 'bundle', 'serve');
+    gulp.watch(['src/**/*'], runSequence('clean', ['copy', 'compile'], 'bundle'), reload);
 });
--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -2,12 +2,11 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { TranslateModule } from 'ng2-translate';
 import { PortfolioComponent } from './portfolio.component';
 
 @NgModule({
     declarations: [PortfolioComponent],
-    imports: [BrowserModule, HttpModule, FormsModule, TranslateModule.forRoot()],
+    imports: [BrowserModule, HttpModule, FormsModule],
     bootstrap: [],
     providers: [HttpModule]
 })
