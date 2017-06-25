35b580c45d7df4b6358c821f4d0380a6a64e1b92
Humakt83 <urlact@yahoo.com>
2017-06-25 07:30:07 +0300

    Display projects

--- src/app/+portfolio/project.ts
+++ src/app/+portfolio/project.ts
@@ -1,9 +1,9 @@
 export class Project {
 
-    public static fromJson(json: any): Project {
-        return new Project(json.name, json.pictures, json.link, json.description, json.tags);
+    public static fromJson(json: any): Project {        
+        return new Project(json.name, json.images, json.link, json.description, json.tags);
     }
 
-    constructor(public name: string, public pictureFiles: string[], public link: string, public description: string, tags: string[]) {}
+    constructor(public name: string, public images: string[], public link: string, public description: string, public tags: string[]) {}
 
 }
