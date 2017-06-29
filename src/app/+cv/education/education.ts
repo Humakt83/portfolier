6d954c8b74f7f2fb17501047fc1f85813f4de3d6
Humakt83 <urlact@yahoo.com>
2017-06-29 20:26:59 +0300

    Format dates with MMM YYYY and utilize momentjs

--- src/app/+cv/education/education.ts
+++ src/app/+cv/education/education.ts
@@ -1,16 +1,18 @@
+import * as moment from 'moment';
+
 export class Education {
 
     public static fromJson(json: any): Education {
-        let ends: Date = json.end ? new Date(json.end) : undefined;
-        return new Education(json.organizer, json.title, json.description, EducationType[<string> json.type], new Date(json.start), ends);
+        let ends: moment.Moment = json.end ? moment(json.end) : undefined;
+        return new Education(json.organizer, json.title, json.description, EducationType[<string> json.type], moment(json.start), ends);
     }
 
     public educationHeader: string = this.formHeader();
 
-    constructor(public organizer: string, public title: string, public description: string, public type: EducationType, public start: Date, public end: Date) { }
+    constructor(public organizer: string, public title: string, public description: string, public type: EducationType, public start: moment.Moment, public end: moment.Moment) { }
 
     private formHeader(): string {
-        return `${this.organizer}, ${!!this.title ? this.title : ''} (${this.start.toLocaleDateString()} - ${this.end ? this.end.toLocaleDateString() : 'continues'})`;
+        return `${this.organizer}, ${!!this.title ? this.title : ''} (${this.start.format('MMM YYYY')} - ${this.end ? this.end.format('MMM YYYY') : 'continues'})`;
     }
 
 }
--- src/app/+cv/experience/experience.ts
+++ src/app/+cv/experience/experience.ts
@@ -1,16 +1,18 @@
+import * as moment from 'moment';
+
 export class Experience {
 
     public static fromJson(json: any): Experience {
-        let ends: Date = json.end ? new Date(json.end) : undefined;
-        return new Experience(json.company, json.position, json.description, new Date(json.start), ends);
+        let ends: moment.Moment = json.end ? moment(json.end) : undefined;
+        return new Experience(json.company, json.position, json.description, moment(json.start), ends);
     }
 
     public experienceHeader: string = this.formHeader();
 
-    constructor(public company: string, public position: string, public description: string, public start: Date, public end: Date) {}
+    constructor(public company: string, public position: string, public description: string, public start: moment.Moment, public end: moment.Moment) {}
 
     private formHeader(): string {
-        return `${this.position}, ${this.company} (${this.start.toLocaleDateString()} - ${this.end ? this.end.toLocaleDateString() : 'continues'})`;
+        return `${this.position}, ${this.company} (${this.start.format('MMM YYYY')} - ${this.end ? this.end.format('MMM YYYY') : 'continues'})`;
     }
 
 }
