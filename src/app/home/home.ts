6d9300c0880306b7f1729b1a748146d58578684a
Humakt83 <urlact@yahoo.com>
2017-06-30 15:10:18 +0300

    Add linkedin

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,9 @@
 export class Home {
 
     public static fromJson(home: any): Home {
-        return new Home(home.title, home.name, home.text, home.showPicture);
+        return new Home(home.title, home.name, home.text, home.linkedIn, home.showPicture);
     }
 
-    constructor(public title: string, public name: string, public text: string[], public showPicture = false) {
+    constructor(public title: string, public name: string, public text: string[], public linkedIn: string, public showPicture = false) {
     }
 }
